cb9a529d6926b630c0354cfa322af68e5191cf45
Sobia <sobiacodes@gmail.com>
2023-07-14 21:48:04 +0100

    song component

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -1,7 +1,20 @@
-import React from "react";
+import {React, useState} from "react";
 
-function Song() {
+function Song({song, playlists, addToPlaylist, addToFavourites}) {
+
+    // state for setting playlist but might move this to SkeletonContainer but seemed relevant to me here
+    const [selectedPlaylist, setSelectedPlaylist] = useState("");
+
+    // handle playlist changingggg function
+
+
+    // handle adding to playlist
+
+    // handle adding to favourites
+
+    //
     return (
+        // put the add to playlist and favourite this song buttons here with the funcs passed as props
         <div>
             
         </div>
