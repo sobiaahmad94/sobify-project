8c32b68b54ead515eaa7d9dac5f9381dbb82abec
Sobia <sobiacodes@gmail.com>
2023-07-14 22:39:40 +0100

    favourites playlist comp functions

--- client/src/components/FavouritePlaylist.js
+++ client/src/components/FavouritePlaylist.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function FavouritePlaylist() {
+function FavouritePlaylist({favourites, removeFromFavourites}) {
     return (
+        //  remove button
         <div>
-            
+      <h2>Favourites:</h2>
+      {favourites.map((song) => (
+        <div key={song._id}>
+          <p>{song.trackName}</p>
+          <button onClick={() => removeFromFavourites(song._id)}>Remove From Favourites</button>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default FavouritePlaylist;
