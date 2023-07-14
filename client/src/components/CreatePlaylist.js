57ac7c08d3e126f3b220b8ef6839dd4df3c3fd2f
Sobia <sobiacodes@gmail.com>
2023-07-14 22:34:48 +0100

    createplaylist component with funcs made

--- client/src/components/CreatePlaylist.js
+++ client/src/components/CreatePlaylist.js
@@ -1,8 +1,27 @@
-import React from "react";
+import {React, useState} from "react";
 
-function CreatePlaylist() {
+function CreatePlaylist({onCreate}) {
+    // playlist name state
+    const [name, setName] = useState("");
+
+    // function that handles input change
+    const handleInputChange = (event) => {
+        setName(event.target.value);
+    };
+
+    // function that handles playlist being created
+    const handleCreate = () => {
+        if (name && name.trim() !== "") {
+            onCreate(name);
+            setName("");
+        }
+    };
+
     return (
+        //input field and create button with props
         <div>
+            <input type="text" value={name} onChange={handleInputChange}/>
+            <button onClick={handleCreate}>Create Playlist</button>
             
         </div>
     );
