72cbea07b9d2de45b112d52784ff6001930a906a
Sobia <sobiacodes@gmail.com>
2023-07-14 22:57:10 +0100

    playlist list comp

--- client/src/components/PlaylistList.js
+++ client/src/components/PlaylistList.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-function PlaylistList() {
+function PlaylistList({ playlists, deletePlaylist, deleteSong }) {
     return (
-        <div>
-            
-        </div>
-    );
-}
+      <div>
+      {/* playlists being shown */}
+      
+        <h2>Playlists</h2>
+        {playlists.map((playlist) => (
+          <div key={playlist._id}>
+            <h3>{playlist.name}</h3>
+            <button onClick={() => deletePlaylist(playlist._id)}> Delete Playlist</button>
 
-export default PlaylistList;
+    {/* songs being shown */}
+            <h4>Songs:</h4>
+            {playlist.songs.map((song) => (
+              <div key={song._id}>
+                <p>{song.trackName}</p>
+                <button onClick={() => deleteSong(playlist._id, song._id)}>Delete Song</button>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default PlaylistList;
