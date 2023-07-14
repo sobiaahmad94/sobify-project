141fa339c32d5b64229db22a9c18e4ce5995c28f
Sobia <sobiacodes@gmail.com>
2023-07-14 22:20:16 +0100

    search bar done

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,9 +1,29 @@
-import React from "react";
+import {React, useState} from "react";
 
-function SearchBar() {
+function SearchBar({ onSearch}) {
+    // keywords state
+    const [keywords, setKeywords] = useState("");
+
+    // handling input change like if something is typed in the input box
+    const handleInputChange = (event) => {
+        setKeywords(event.target.value);
+    };
+
+
+    // handling search result
+    const handleSearch = (event) => {
+        event.preventDefault() // added this line because as soon as I typed a character it was registering each character 
+        onSearch(keywords);
+    };
+
+    
     return (
+        // form needs to go here, input and search button
         <div>
-            
+            <form onSubmit={handleSearch}>
+                <input type="text" value={keywords} onChange={handleInputChange}/>
+                <button type="submit">Search</button>
+            </form>
         </div>
     );
 }
