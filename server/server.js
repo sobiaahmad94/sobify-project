1f7494e7fc258eb7641f9b8de13d6751cc00e4ca
Sobia <sobiacodes@gmail.com>
2023-07-15 13:27:36 +0100

    set up backend server

--- server/server.js
+++ server/server.js
@@ -7,8 +7,29 @@ const cors = require("cors");
 const app = express();
 app.use(express.json);
 
-const mongodbLink = "mongodb://localhost:27017/"
+// sobify-music is the database 
+const mongodbLink = "mongodb://localhost:27017/sobify-music";
 
-app.listen(9000, () => {
-    console.log(`the server's running here on http://localhost:${backEndPort}`);
+// connecting to mongoose:)
+mongoose.connect(mongdbLink, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
 })
+.then(() => {
+    const backEndPort = 9000;
+
+    app.listen(backEndPort, () => {
+        console.log(`the server is running on port number ${backEndPort}`);
+        console.log("I'm connected to mongoDB right now");
+    });
+})
+.catch((error) => {
+    console.error("oh no, failed to connect to mongodb", error);
+});
+
+// importing and using routes which'll be in reference to my other folder
+// routes
+
+// do app.use for them after
+
+
