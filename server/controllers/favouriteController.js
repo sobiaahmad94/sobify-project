c6bf2345bf79551dc422c823280b3e192c35fe9d
Sobia <sobiacodes@gmail.com>
2023-07-15 17:13:38 +0100

    more favourite controller funcs done

--- server/controllers/favouriteController.js
+++ server/controllers/favouriteController.js
@@ -1,8 +1,23 @@
 const Playlist = require("../models/playlistModel");
 
 // getting the favourites playlist
+const getFavouritesPlaylist = async (req, res) => {
+    try {
+        const playlist = await Playlist.findOne({name: "favourites playlist"});
+
+        if (!playlist) {
+            return res.status(404).json({error: "favourites playlist not found"});
+        }
+
+        res.json(playlist);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "failed to get favourites playlist"});
+    }
+};
 
 // adding a song to the favourites playlist
+
 
 // getting all of the songs that are in the favourites playlist
 
