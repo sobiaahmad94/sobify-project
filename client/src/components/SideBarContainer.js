91c1f1cdd9f600d2551b1feb3709fd299d76bbbf
Sobia <sobiacodes@gmail.com>
2023-07-15 11:39:02 +0100

    sidebarcontainer done

--- client/src/components/SideBarContainer.js
+++ client/src/components/SideBarContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function SideBarContainer() {
+import CreatePlaylist from "./CreatePlaylist";
+import PlaylistList from "./PlaylistList";
+import FavouritePlaylist from "./FavouritePlaylist";
+
+// pass in my props to refactor so CreatePlaylist, PlaylistList, FavouritePlaylist are all inside SideBarContainer
+function SideBarContainer({playlists, deletePlaylist, deleteSong, favourites, removeFromFavourites, createPlaylist}) {
     return (
         <div>
+            <CreeatePlaylist onCreate={createPlaylist}/>
+            <PlaylistList playlists={playlists} deletePlaylist={deletePlaylist} deleteSong={deleteSong}/>
+            <FavouritePlaylist favourites={favourites} removeFromFavourites={removeFromFavourites}/>
             
         </div>
     );
-}
+};
 
 export default SideBarContainer;
