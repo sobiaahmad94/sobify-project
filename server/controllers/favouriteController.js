24616fb0717b332410f594e18dafc011ad28df1d
Sobia <sobiacodes@gmail.com>
2023-07-15 17:03:44 +0100

    structuring what funcs I need to create in favourite controller

---
+++ server/controllers/favouriteController.js
@@ -0,0 +1,10 @@
+const Playlist = require("../models/playlistModel");
+
+// getting the favourites playlist
+
+// adding a song to the favourites playlist
+
+// getting all of the songs that are in the favourites playlist
+
+// removing a song from the favourites playlist
+
