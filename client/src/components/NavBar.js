47ea746e946a4daa00486ecb4919ea2cb0992df7
Sobia <sobiacodes@gmail.com>
2023-07-16 13:42:46 +0100

    sobify img added to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import sobifyNavLogoImg from "../images/sobify-logo-black-background.jpeg";
 
 function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component={Link} to="/" style={{ flexGrow: 1 }}>
-          Sobify
-        </Typography>
-        <Button color="rgba(4, 22, 52)" component={NavLink} exact to="/about" activeClassName="active">
+        <Link to="/" style={{ textDecoration: "none" }}>
+            <img src={sobifyNavLogoImg} alt="Sobify nav logo" style={{height: 40, marginRight: 10}}/>
+          
+        </Link>
+        <Button color="inherit" component={NavLink} exact to="/login" activeClassName="active">
           Login
         </Button>
-        <Button color="inherit" component={NavLink} exact to="/contact" activeClassName="active">
+        <Button color="inherit" component={NavLink} exact to="/account" activeClassName="active">
           Account
         </Button>
       </Toolbar>
