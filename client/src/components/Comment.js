77f8fdaa9f366f45fc8c92df3764c3c6901c2610
Sobia <sobiacodes@gmail.com>
2023-07-18 14:39:40 +0100

    comment router done

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -9,7 +9,7 @@ function Comment() {
     fetchComments();
   }, []);
 
-  // fetching the comments
+  // fetching the comments func
   const fetchComments = async () => {
     try {
     const response = await api.get(`/comments/${songId}`);
@@ -19,17 +19,34 @@ function Comment() {
     }
     };
 
-    // adding comment
+    // add comment func
+    const addComment = async () => {
+      try {
+      const response = await api.post(`/comments/${songId}`, { text: newComment });
+      setComments([...comments, response.data]);
+      
+      setNewComment("");
 
+      } catch (error) {
+      console.error("failed to add comment", error);
+      }
+      };
   
-    
 };
 
 
 return (
   <div>
+    <textarea rows="4" cols="40" value={newComment} 
+    onChange={(event) => setNewComment(event.target.value)}></textarea>
 
+    <button onClick={addComment}>Add Comment</button>
 
+    <ul> 
+    {comments.map((comment) => (
+    <li key={comment._id}>{comment.text}</li>
+    ))}
+    </ul>
   </div>
 )
 
