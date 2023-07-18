a32a720b947f45e652a9761822ea5357189e6be2
Sobia <sobiacodes@gmail.com>
2023-07-18 13:46:12 +0100

    fetching comments func

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -2,10 +2,33 @@ import React, {useState, useEffect} from "react";
 import api from "../services/api";
 
 function Comment() {
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
-}
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  // fetching the comments
+  const fetchComments = async () => {
+    try {
+    const response = await api.get(`/comments/${songId}`);
+    setComments(response.data);
+    } catch (error) {
+    console.error("failed to fetch comments", error);
+    }
+    };
+
+    // adding comment
+
+  
+    
+};
+
+
 return (
   <div>
+
 
   </div>
 )
