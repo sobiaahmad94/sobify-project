c1eae9e7bbb2c74cc54b2ba76dcc4d0393987f5d
Sobia <sobiacodes@gmail.com>
2023-07-19 02:38:20 +0100

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import SkeletonContainer from "./components/SkeletonContainer";
+import {BrowserRouter as Router} from "react-router-dom";
+
+
+// styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <SkeletonContainer />
+      </Router>
 
 
     
