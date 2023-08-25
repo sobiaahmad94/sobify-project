ff274e1e590a1f8bc38380ac18ecae92d0d94872
Sobia <sobiacodes@gmail.com>
2023-08-25 16:05:13 +0100

    loading feature added to make searching more user friendly

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -26,28 +26,6 @@ const StyledSearchResultsBox = styled.div`
   width: 100%;
 `;
 
-// function SearchResults({ searchResults, playlists, addToPlaylist, addToFavourites, loading }) {
-//   return (
-//     <StyledSearchResultsContainer>
-//       <StyledSearchResultsBox>
-//         <h1>Search Results</h1>
-//         {loading ? (
-//           <span>Loading results...</span>
-//         ) : (
-//           searchResults.map((song, index) => (
-//             <div key={`${song.songId}-${index}`}>
-//               <Song song={song} playlists={playlists} addToPlaylist={addToPlaylist} addToFavourites={addToFavourites}/>
-//               {/* <Comment songId={song.songId}/>  */}
-//             </div>
-//           ))
-//         )}
-//       </StyledSearchResultsBox>
-//     </StyledSearchResultsContainer>
-//   );
-// }
-
-// export default SearchResults;
-
 function SearchResults({ searchResults, playlists, addToPlaylist, addToFavourites, loading }) {
   return (
     <StyledSearchResultsContainer>
